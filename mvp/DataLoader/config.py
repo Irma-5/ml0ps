88003fd67b55dc16e725f9d75f824b8412b53c11
BigMilk37 <comradeduck399@gmail.com>
2025-05-19 09:58:20 +0300

    Update config.py

--- mvp/DataLoader/config.py
+++ mvp/DataLoader/config.py
@@ -1,7 +1,9 @@
 # config.py
-
+import json
+with open('config.json', 'r') as f:
+    CONFIG = json.load(f)
 DATA_LOADER_PARAMS = {
     "path": r"DataLoader",  # Путь к модулю данных
-    "year_to_split": 2010,  # Год, по которому разделяем данные
-    "num_batch": 10,  # Количество кварталов для обработки
+    "year_to_split":CONFIG["year_to_split"],  # Год, по которому разделяем данные
+    "num_batch": CONFIG["num_batch"],  # Количество кварталов для обработки
 }
