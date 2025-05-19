2bc3d36855292c55fe8072e90dc3d8fab21cc78e
BigMilk37 <comradeduck399@gmail.com>
2025-05-19 12:22:38 +0300

    Update config.py

--- mvp/DataLoader/config.py
+++ mvp/DataLoader/config.py
@@ -1,9 +1,9 @@
-# config.py
-import json
-with open('model_artifacts/config.json', 'r') as f:
-    CONFIG = json.load(f)
+# # config.py
+# import json
+# with open('model_artifacts/config.json', 'r') as f:
+#     CONFIG = json.load(f)
 DATA_LOADER_PARAMS = {
     "path": r"DataLoader",  # Путь к модулю данных
-    "year_to_split":CONFIG["year_to_split"],  # Год, по которому разделяем данные
-    "num_batch": CONFIG["num_batch"],  # Количество кварталов для обработки
+    "year_to_split": 2010, 
+    "num_batch": 10# Количество кварталов для обработки
 }
